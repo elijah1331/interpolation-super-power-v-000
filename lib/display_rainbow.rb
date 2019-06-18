7f2c14fdee1674ace117e9e514431729e340ac7b
Elijah Marcano <emarcano143@hotmail.com>
2019-06-18 14:28:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,6 @@
 def display_rainbow(array_of_colors)
   puts "R: #{array_of_colors[0]}, O: #{array_of_colors[1]},"
   puts "Y: #{array_of_colors[2]}, G: #{array_of_colors[3]},"
-        B: #{array_of_colors[4]}, I: #{array_of_colors[5]},
-        V: #{array_of_colors[6]}"
+  puts "B: #{array_of_colors[4]}, I: #{array_of_colors[5]},"
+  puts "V: #{array_of_colors[6]}"
 end
